0b8f3e7afbfd1f4ee16ed3da9de5d03519c1f15f
ohdooong <ohseung1486@gmail.com>
2026-06-21 22:55:57 +0900

    feat: 공통응답형식 설정

--- backend/streamcell-api/streamcell/src/main/java/com/streamcell/global/common/dto/BaseResponse.java
+++ backend/streamcell-api/streamcell/src/main/java/com/streamcell/global/common/dto/BaseResponse.java
@@ -5,12 +5,31 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Setter
 @Getter
-@AllArgsConstructor(staticName = "of")
+@AllArgsConstructor
 public class BaseResponse<T> {
 
+    private int status;
+    private String message;
+    private LocalDateTime timestamp;
+    private T body;
 
+    public static <T> BaseResponse<T> success(String message, T body) {
+        return new BaseResponse<>(200, message, LocalDateTime.now(), body);
+    }
 
+    public static <T> BaseResponse<T> success(T body) {
+        return new BaseResponse<>(200, "요청 성공", LocalDateTime.now(), body);
+    }
 
+    public static <T> BaseResponse<T> error(int status, String message, T body) {
+        return new BaseResponse<>(status, message, LocalDateTime.now(), body);
+    }
+
+    public static <T> BaseResponse<T> error(String message) {
+        return new BaseResponse<>(500, message, LocalDateTime.now(), null);
+    }
 }
