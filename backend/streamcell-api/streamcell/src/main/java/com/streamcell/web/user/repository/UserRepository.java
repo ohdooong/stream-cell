497a3f5128d6e8d38afa040122af408f3d90306c
ohdooong <ohseung1486@gmail.com>
2026-06-25 20:27:08 +0900

    feat: 사용자 검증 로직 추가

--- backend/streamcell-api/streamcell/src/main/java/com/streamcell/web/user/repository/UserRepository.java
+++ backend/streamcell-api/streamcell/src/main/java/com/streamcell/web/user/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.streamcell.web.user.repository;
 
 import com.streamcell.web.user.domain.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +13,12 @@ public interface UserRepository {
 
     List<User> findAll();
 
+    List<Long> findExistingUserIds(List<Long> userIds);
+
+    @Select("""
+        select count(1)
+        from web.users
+        where user_id = #{userId}
+    """)
+    int existsByUserId(Long userId);
 }
