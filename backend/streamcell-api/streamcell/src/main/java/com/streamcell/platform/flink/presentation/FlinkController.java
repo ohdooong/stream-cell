55b6b3e38db2574c119f488639a32fdf0179932b
seunghwan.oh13 <ohseung1486@gmail.com>
2026-07-27 18:54:37 +0900

    resolve merge conflict: Flink Controller

---
+++ backend/streamcell-api/streamcell/src/main/java/com/streamcell/platform/flink/presentation/FlinkController.java
@@ -0,0 +1,49 @@
+package com.streamcell.platform.flink.presentation;
+
+import com.streamcell.global._common.dto.BaseResponse;
+import com.streamcell.global._common.enums.ErrorCode;
+import com.streamcell.global._common.exception.BaseAPIException;
+import com.streamcell.platform.flink.dto.FlinkResponse;
+import com.streamcell.platform.flink.service.FlinkService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "Flink API", description = "Flink API 컨트롤러")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/platform/flink")
+public class FlinkController {
+
+    private final FlinkService service;
+
+    @Operation(summary = "Flink Cluster Info 조회", description = "Flink Cluster의 상세정보를 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회성공"),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error."),
+    })
+    @GetMapping("/cluster-overview")
+    public ResponseEntity<BaseResponse<FlinkResponse.ClusterOverview>> getClusterOverview() {
+        FlinkResponse.ClusterOverview response = service.getClusterOverview();
+        return ResponseEntity.ok(BaseResponse.success(response));
+    }
+
+    @Operation(summary = "Flink Cluster에 등록된 jar run", description = "Flink Cluster에 등록된 jar를 실제로 run한다.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "성공"),
+        @ApiResponse(responseCode = "400", description = "Bad Request"),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error."),
+    })
+    @PostMapping("/jars/{flinkJarId}/run")
+    public ResponseEntity<BaseResponse<?>> runFlinkJarById(
+        @PathVariable String flinkJarId
+    ) {
+
+        return ResponseEntity.ok(null);
+    }
+}
